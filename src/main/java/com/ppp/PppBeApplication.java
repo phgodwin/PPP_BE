e3c6e3c59ba3f3e77479c217077b164e4d3fd3a4
Sailesh-Patel <150167583+Sailesh-Patel@users.noreply.github.com>
2024-05-07 11:27:56 +0100

    initial commit

---
+++ src/main/java/com/ppp/PppBeApplication.java
@@ -0,0 +1,13 @@
+package com.ppp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PppBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppp/PppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
